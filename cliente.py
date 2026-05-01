83c2da5bc2dc7c58869f01941f51a9f8156f02f9
Paul David Delgado <paulddaviddv19@gmail.com>
2026-04-30 22:17:36 -0500

    actualiacion de datos

--- cliente.py
+++ cliente.py
@@ -9,11 +9,14 @@ def separador(titulo: str):
     print(f"{'*'*50}")
 
 def mostrar(resp):
-    print("Status: {resp.status_code}")
-    if resp.status_code:
-        print("body: {json.dumps(resp.json(), indent=2, ensure_ascii=False)}")
-    else:
-        print("body: {resp.text}")
+    print(f"Status: {resp.status_code} | OK: {resp.ok}")
+    try:
+        body = resp.json()
+        print(f"body: {json.dumps(body, indent=2, ensure_ascii=False)}")
+    except ValueError:
+        print(f"body: {resp.text}")
+    except requests.exceptions.ConnectionError:
+        print("No se pudo establecer conexion con el servidor")
 
 separador("GET / - Raiz")
 mostrar(requests.get(BASE + "/"))
@@ -22,8 +25,35 @@ separador("GET /estudiantes")
 mostrar(requests.get(BASE + "/estudiantes"))
 
 separador(" GET /estudiantes/1 - Por id" )
-mostrar(requests.get(BASE + "/estudiantes/1/"))
+mostrar(requests.get(BASE + "/estudiantes/1"))
 
-if __name__ == "__main__":
-    print("Servidor iniciado")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     print("Servidor iniciado")
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+# import json
+# import requests
+
+
+# BASE = "http://localhost:8000"
+
+
+# def separador(titulo: str):
+#     print(f"\n {'-'*50}")
+#     print(f"{titulo}")
+#     print(f"\n {'-'*50}")
+
+
+# def mostrar(resp):
+#     if resp.status_code:
+#         print(f"Body: {json.dumps(resp.json(), indent=2, ensure_ascii=False)}")
+
+
+# separador("GET / - Raiz")
+# mostrar(requests.get(f"{BASE}/"))
+
+# separador("GET / - Estudiantes")
+# mostrar(requests.get(f"{BASE}/students"))
+
+# separador("GET / - Estudiante ID 1")
+# mostrar(requests.get(f"{BASE}/students/1/"))
